de328ab8d1aeaf7f605fe0bc760479f348997885
hinton <spotify233833@gmail.com>
2026-09-06 09:39:18 +0530

    Add ContactManager class with basic functionality

--- EmergencyContactSystem/ContactManager.h
+++ EmergencyContactSystem/ContactManager.h
@@ -1,1 +1,25 @@
+#ifndef CONTACTMANAGER_H
+#define CONTACTMANAGER_H
 
+#include "Contact.h"
+#include <vector>
+using namespace std;
+
+class ContactManager {
+
+private:
+    vector<Contact> contacts;
+
+public:
+
+    ContactManager();
+
+    void addContact();
+    void displayContacts();
+    void searchContact();
+    void deleteContact();
+
+    void saveData();
+};
+
+#endif
